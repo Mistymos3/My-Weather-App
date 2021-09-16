3b93302d2f1df4ca359923ea8c8ea3965466dcfc
Iva <84078385+Mistymos3@users.noreply.github.com>
2021-09-16 02:06:01 -0700

    updated code...got forecast to work, icon on todays temp div works now

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-const date = moment().format("dddd, MMMM Do YYYY");
+const date = moment().format("dddd, MMMM Do, YYYY");
 // console.log(date);
-
-
 
 //openweathermap.org API key
 const apiKey = '61371caa7f5d756c79ef18bb6118aec1'
@@ -21,13 +19,12 @@ const apiKey = '61371caa7f5d756c79ef18bb6118aec1'
 // console.log(queryURL);
 // console.log(UVIqueryURL);
 
-var longitute
-var latitude
+// var longitute
+// var latitude
 
 
 //put DATE ON PAGE
 $("#todaysDate").html(date);
-
 
 
 //CURRENT WEATHER FUNCTION ... adding in uv function inside current weather function
@@ -52,12 +49,20 @@ var lon = (response.coord.lon)
 // longitute = lon;
 
 
+//Gets the icon for the current weather
+var icon = response.weather[0].icon;
+var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
+// http://openweathermap.org/img/w/04d.png
+//sets the icon 
+$("#icon").attr('src', iconUrl);
+
+
 
       // Constructing HTML containing weather information
       $("#city").text(response.name);
       $("#mainWeather").text(response.weather[0].main);
       $("#weatherDescription").text(response.weather[0].description);
-      // $("icon").attr('src', buildIconUrl(response.weather[0].icon));
+    //   $("#icon").attr('src', (response.weather[0].icon))
       //TEMPERATURE CONVERSION FROM KELVIN TO F
       var tempF = (response.main.temp - 273.15) * 1.80 + 32;
       $("#todaysTemp").html(tempF.toFixed(2) + " &deg;F");
@@ -66,22 +71,21 @@ var lon = (response.coord.lon)
   
 
 //SAVE TO LOCAL STORAGE
-    //   localStorage.setItem("weather", response.name)
+      localStorage.setItem("weather", response.name)
+      localStorage.setItem("lat", response.coord.lat)
+      localStorage.setItem("lon", response.coord.lon)
 
 
-
-              
-        // localStorage.setItem("lat", response.coord.lat)
-        // localStorage.setItem("lon", response.coord.lon)
 UVindex(lat, lon);
-
+ fiveDayForecast(response);
+// fiveDayForecast(city)
             });
         }  
 
+
         //UV INDEX FUNCTION
         function UVindex(lat, lon) {
-                        //    lat=33.44&lon=-94.04&exclude=hourly,daily&appid=61371caa7f5d756c79ef18bb6118aec1
-            // var uvIndexURL = "https://api.openweathermap.org/data/2.5/onecall?appid=" + apiKey + "&" + lat + "=&" + lon + "=&units=imperial"
+            
             var uvIndexUrl = "https://api.openweathermap.org/data/2.5/onecall?&appid=" + apiKey + "&lat=" + lat + "&lon=" + lon
             //AJAX CALL
             $.ajax({
@@ -119,6 +123,10 @@ UVindex(lat, lon);
                         $("#uvColor").attr('style', 'background-color: #0206fd5b;')
                     }
                 
+                for (var i = 1; i <= 5; i++) {             
+                fiveDayForecast(i, response1.daily[i]);
+                };
+               
             })
 
         }
@@ -128,34 +136,17 @@ UVindex(lat, lon);
 
 
 
+function fiveDayForecast(i, forecast) {
+    console.log(forecast);
 
-
-
-//5 DAY FORECAST
-
-function FIVEDayForcast(city) {
-    var fivedayURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey
-
-        //AJAX Call
-        $.ajax({
-        url: fivedayURL,
-        method: "GET"
-        }).then(function(response5Day) {
-            console.log(response5Day);
-
-
-
-        // var 
-        });
-
-    }
-
-
-
-
-
-
-
+    // let date = moment(forecast.dt_txt).format("MMMM Do, YYYY");
+    $(`#date${i}`).text(date)
+    // $(`#icon${i}`).attr("src", icon(forecast.weather[0].icon))
+    var tempF = (forecast.temp.day - 273.15) * 1.80 +32;
+    $(`#temp${i}`).html(`${tempF.toFixed(2)} ℉`)
+    $(`#hum${i}`).html(`${forecast.humidity} %`)
+    $(`#wind${i}`).html(`${forecast.wind_speed} MPH`)
+}
 
 
 
@@ -176,78 +167,6 @@ $("#search-button").on("click", function(event) {
 
 
   
-
-
-
-
-
-
-
-
-
-    
-//   var cityInput = $("#search-value").val().trim();
-  
-//   //variable that stores the Open Weather Map API for 5 day forecasts 
-//   var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
-
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response){
-//     $("#5day").text("5-Day Forecast:")
-    
-  
-//     //for loop that checks the data gotten from the API and returns items that include the time 15:00. 
-//     //This ensures that only 1 hour is included per day, giving us our 5 day forecast.
-//     for (var i=0; i<response.list.length; i++){
-//       if (response.list[i].dt_txt.includes("15:00:00")){
-//       //pushes the indexes that include 15:00 to the forecastArray
-//       forecastArray.push(i);
-//       //Gets the date for each included index 
-//       var dates = response.list[i].dt_txt;
-//       //formats the dates wiht the Moment API 
-//       var datesFormat = moment(dates);
-//       var datesFormat = moment(dates).format("MMMM/Do/YYYY");
-//       //gets the components for each index and assingns them to a variable 
-//       var datesTemp = response.list[i].main.temp;
-//       var datesHumidity = response.list[i].main.humidity;
-//       var datesIcon = response.list[i].weather[0].icon;
-//       var iconURL = "http://openweathermap.org/img/w/" + datesIcon + ".png";
-
-//         //creates cards, using the bootstrap template, that inlcude the date, icon, temperature, and humidity 
-//       $("#cards").append(
-//         '<div id="card' + i + '" class="card bg-info mx-3" style="width: 10rem;"><div class="card-body"><h5 class="card-title">Card title</h5><img src="" alt="Weather Icon"><p class="card-text temp">Temp: </p><p class="card-text humidity">Humidity: </p></div></div>'
-//       );
-//       //sets the text of each title to be the date for each card
-//       $("#card"+i+" h5").text(datesFormat);
-//       //appends the temperature for each card
-//       $("#card"+i).find(".temp").append(datesTemp + "℉");
-//       //appends the humidity for each card
-//       $("#card"+i).find(".humidity").append(datesHumidity+"%");
-//       //appends the icon for each card 
-//       $("#card"+i).find("img").attr("src",iconURL);
-   
-//     }}
-//     //stores the 5day forecast data in local storage 
-//     localStorage.setItem("5Day", JSON.stringify(response));
-      
-//   })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
